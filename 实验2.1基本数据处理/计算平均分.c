991a01cd6c6dd6ca3edaa851b229af9c9999a2cc
Wunsuol <89762330+Wunsuol@users.noreply.github.com>
2021-09-20 09:16:22 +0800

    up my schoolwork
    this is my second time to up my schoolwork
    2021 9.20

---
+++ 实验2.1基本数据处理/计算平均分.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int math=87,eng=72,comp=93,average;
+    average=(math+eng+comp)/3;
+    printf("math = %d, eng = %d, comp = %d, average = %d",math,eng,comp,average);
+    return 0;
+    
+}
