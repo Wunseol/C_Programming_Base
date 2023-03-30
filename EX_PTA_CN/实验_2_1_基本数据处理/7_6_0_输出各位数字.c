7174b7ff5c1ee8d80dd48f5f2bc56fa807b87a5c
Wunsuol <89762330+Wunsuol@users.noreply.github.com>
2023-03-30 19:23:40 +0800

    Add files via upload

---
+++ EX_PTA_CN/实验_2_1_基本数据处理/7_6_0_输出各位数字.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int amount,a,b,c;
+    scanf("%d",&amount);
+    a=amount/100;
+    b=((amount-a*100)-(amount-a*100)%10)/10;
+    c=(amount-a*100-b*10);
+    printf("%d,%d,%d",a,b,c);
+    return 0;
+
+}
