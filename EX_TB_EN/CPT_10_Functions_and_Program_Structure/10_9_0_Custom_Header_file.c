93ae8872cb4d00506ba7bc091fa3fe891ebc3879
Wunsuol <89762330+Wunsuol@users.noreply.github.com>
2023-07-17 16:46:46 +0800

    Add files via upload

---
+++ EX_TB_EN/CPT_10_Functions_and_Program_Structure/10_9_0_Custom_Header_file.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "C:\VS Code\C_Programming\C_Language\C_Programming_Base\EX_TB_EN\CPT_10_Functions_and_Program_Structure\Custom_header_file\length.h"
+// #include "" 表示在当前文件夹查找，找不到的话就直接写入路径
+
+int main()
+{
+    double foot, inch, mile;
+    printf("Input mile , foot and inch:");
+    scanf("%lf %lf %lf", &mile ,&foot ,&inch);
+    printf("%lf miles = %lf meter \n", mile, mile*Mile_to_meter);
+    printf("%lf feet = %lf centimeter \n", foot, foot*Foot_to_centimeter);
+    printf("%lf inches = %lf centimeter \n", inch, inch*Inch_to_centimeter);
+    
+    return 0;
+
+}
